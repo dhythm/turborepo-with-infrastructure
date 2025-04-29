81f9f5b8a109534e4c9f47fa5ad6cc2e8c7a70fd
dhythm <yuta.okada.20@gmail.com>
2025-04-29 21:48:06 +0900

    update infra/main.ts to add GoogleProvider

--- packages/infra/main.ts
+++ packages/infra/main.ts
@@ -1,11 +1,32 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { GoogleProvider } from "@cdktf/provider-google/lib/provider";
+import { ComputeInstance } from "@cdktf/provider-google/lib/compute-instance";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
+    new GoogleProvider(this, "google", {
+      project: "your-gcp-project-id",
+      region: "us-central1",
+      zone: "us-central1-a",
+    });
+
+    new ComputeInstance(this, "vm-instance", {
+      name: "cdktf-vm",
+      machineType: "e2-micro",
+      zone: "us-central1-a",
+      bootDisk: {
+        initializeParams: {
+          image: "debian-cloud/debian-11",
+        },
+      },
+      networkInterface: [{
+        network: "default",
+        accessConfig: [{}],
+      }],
+    });
   }
 }
 
